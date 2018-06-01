b3ca68e48baeef8d6374dcaaeb4b95fcd7ad2f9b
Sophiequehen <sophie.quehen@gmail.com>
2018-06-01 15:49:02 +0200

    Chat fonctionnel avec les noms des utilisateurs et la mise en forme pour celui qui écrit

--- public/dist/js/client.js
+++ public/dist/js/client.js
@@ -1,15 +1,38 @@
 $(function () {
-  var socket = io();
+	var socket = io();
 
          // On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre
          var pseudo = prompt('Quel est votre pseudo ?');
          
+         if(pseudo == ''){
+         	pseudo = 'Un chateur anonyme';
+         }
          socket.emit('nouveau_client_vers_server', pseudo);
-          socket.emit('disconnect',pseudo);
-         
+         socket.on('nouveau_client_vers_client', function(pseudo){
+         	$('#messages').append('<li><em>'+ pseudo + ' est maintenant connecté(e) !</em></li>')
+         });
 
-      
-          
-          
+         socket.emit('disconnect',pseudo);
+         socket.on('bye_client_vers_client', function(pseudo){
+         	$('#messages').append('<li><em>'+ pseudo + ' est maintenant déconnecté(e) !</em></li>')
+         });
 
-    });
+         $('form').submit(function(){
+         	var input = $('#m').val();
+         	if(input !== ''){	
+         		socket.emit('msg_vers_server', pseudo, input);
+         	// Affiche le message aussi sur notre page
+         	$('#messages').append('<p class="mes-messages">'+ input +'</p>');
+         	$('#m').val('').focus(); // Vide la zone de Chat et remet le focus dessus 
+         	return false;//pour éviter que la page ne se recharge à chaque submit
+         }else{
+         	return false;
+         }
+     });
+
+          // Quand on reçoit un message, on l'insère dans la page
+          socket.on('msg_vers_client', function(data) {
+          	$('#messages').append('<p><strong>' + data.pseudo + '</strong></p><p> '+ data.msg +'</p>');
+          });
+
+      });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var compteur = 0;
 
 //création et liaison avec le dossier public
 app.use(express.static('public'));
@@ -13,25 +14,34 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-	 // Dès qu'on nous donne un pseudo, on le stocke en variable de session
-	  // et on informe les autres personnes
-	  socket.on('nouveau_client_vers_server', function(pseudo) {
+	// Dès qu'on nous donne un pseudo, on le stocke en variable de session
+	// et on informe les autres personnes
+	socket.on('nouveau_client_vers_server', function(pseudo) {
+	  	//pour qu'il le garde en mémoire, comme il est stocké dans socket, pas besoin de le redéfinir dans les fonctions d'après
 	  	socket.pseudo = pseudo;
-
-	  	console.log(socket.pseudo+' connecté ')
+	  	compteur ++;
+	  	console.log(compteur);
+	  	socket.emit('nouveau_client_vers_client', pseudo);
+	  	//broadcast pour que tous les autres sur le serveur le voit
+	  	socket.broadcast.emit('nouveau_client_vers_client', pseudo);
 	  });
 
-	  
-
-    socket.on('disconnect', function(pseudo){
-	
+	//Quand un client se déconnecte
+	socket.on('disconnect', function(pseudo){
+		compteur --;
+		console.log(compteur);
 		console.log(socket.pseudo+' disconnected');
-		
+		socket.emit('bye_client_vers_client', socket.pseudo);
+	  	//broadcast pour que tous les autres sur le serveur le voit
+	  	socket.broadcast.emit('bye_client_vers_client', socket.pseudo);
+	  });
+
+	//Quand un message est submit
+	socket.on('msg_vers_server', function(pseudo, msg){
+		// socket.msg = msg;
+		socket.broadcast.emit('msg_vers_client', {msg:msg, pseudo:socket.pseudo});
+		// console.log(msg);
 	});
-
-
-
-
 });
 
 http.listen(3000, function(){
